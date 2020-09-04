84fbe6e65a0a5b659d0e09186f7cc008f0d718e8
Enderw12 <enderw2012@gmail.com>
2020-09-04 22:34:58 +0300

    комментарии

--- converter.py
+++ converter.py
@@ -1,7 +1,11 @@
 import subprocess
-
+# subprocess позволяет обращаться к системным библиотекам на языке shell скриптов
 
 def convert(inputFileName, outputFileName):
+    # обращаемся к библиотеке ddjvu которая умеет конвертировать djvu в pdf 
+    # качество изображений на 60% из возможных 140%
+    # передаём имя (оно же путь к файлу который нужно конвертировать)
     result = subprocess.run(
         ['ddjvu', '-quality=60', '-format=pdf', inputFileName, outputFileName, ])
+    # возвращаем имя файла по завершению конвертации (оно же и путь, ибо файл находится в той же папке что и программа)
     return result.args[-1]
